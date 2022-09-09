2b647d1e23de36eb9cb81b341413636cf72c3f76
Salamatu Umar <SalamatuUmarAliyu35@gmail.com>
2022-09-09 10:15:32 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return:0
+ */
+int main(void)
+{
+	int i, y;
+	for (i = 0; i < 10; i++)
+	{
+		putchar (i + '0');
+	}
+	putchar (i + '0');
+	{
+		putchar (y);
+	}
+	
+putchar('\n');
+return (0);
+}
