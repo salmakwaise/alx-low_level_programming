ee57fb2f2c69f9fe9982493682542f931bc6ffc6
Salamatu Umar <SalamatuUmarAliyu35@gmail.com>
2022-09-10 03:08:28 -0700

    100100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - print single digit numbers
+ *
+ * Description: print single digit numbers with commas
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j;
+
+	i = 48;
+	j = 48;
+
+	while (i < 58)
+	{
+		j = 48;
+		while (j < 58)
+		{
+			putchar(i);
+			putchar(32);
+		}
+		j++;
+	}
+
+	i++;
+}
+
+putchar(10);
+
+return (0);
+}
+
+
